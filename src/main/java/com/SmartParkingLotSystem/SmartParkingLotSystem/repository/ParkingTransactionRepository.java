6cae0a6234fa0406f06c7db64fadc040f9ed39c4
archis1405 <archismandas1405@gmail.com>
2025-10-19 01:09:21 +0530

    Added ParkingTransaction and the Vehicle Repository

---
+++ src/main/java/com/SmartParkingLotSystem/SmartParkingLotSystem/repository/ParkingTransactionRepository.java
@@ -0,0 +1,22 @@
+package com.SmartParkingLotSystem.SmartParkingLotSystem.repository;
+
+import com.SmartParkingLotSystem.SmartParkingLotSystem.entity.ParkingTransactions;
+import com.SmartParkingLotSystem.SmartParkingLotSystem.models.enums.TransactionStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ParkingTransactionRepository extends JpaRepository<ParkingTransactions , Long> {
+
+    @Query("SELECT pt FROM ParkingTransactions pt WHERE pt.vehicle.licensePlate = :licensePlate AND pt.transactionStatus = :status ORDER BY pt.checkInTime DESC LIMIT 1")
+    Optional<ParkingTransactions> findActiveTransactionByLicensePlate(@Param("licensePlate") String licensePlate , @Param("status")TransactionStatus status);
+
+    @Query("SELECT pt FROM ParkingTransactions pt WHERE pt.checkInTime BETWEEN :startTime AND :endTime")
+    List<ParkingTransactions> findTransactionsByDateRange(@Param("startTime")LocalDateTime startTime , @Param("endTime") LocalDateTime endTime);
+}
