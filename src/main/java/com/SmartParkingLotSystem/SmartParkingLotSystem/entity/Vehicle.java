f44274a506301e70f995d53837bbd7d6bf300c21
archis1405 <archismandas1405@gmail.com>
2025-10-18 20:19:06 +0530

    Added the Vehicle Entity

---
+++ src/main/java/com/SmartParkingLotSystem/SmartParkingLotSystem/entity/Vehicle.java
@@ -0,0 +1,50 @@
+package com.SmartParkingLotSystem.SmartParkingLotSystem.entity;
+
+import com.SmartParkingLotSystem.SmartParkingLotSystem.models.enums.VehicleType;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity
+@Table(name = "vehicles" , uniqueConstraints = @UniqueConstraint(columnNames = "license_plate"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "license_plate" , nullable = false , unique = true)
+    private String licensePlate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "vehicle_type" , nullable = false)
+    private VehicleType vehicleType;
+
+    @Column(name = "colour" , nullable = false)
+    private String colour;
+
+    @Column(name = "model" , nullable = false)
+    private String model;
+
+    @OneToMany(mappedBy = "vehicle", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<ParkingTransaction> transactions;
+
+    @Column(name = "created_at" , nullable = false , updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at" , nullable = false)
+    private LocalDateTime updatedAt;
+
+    protected void onCreate(){
+        createdAt=LocalDateTime.now();
+        updatedAt=LocalDateTime.now();
+    }
+
+    protected void onUpdate(){
+        updatedAt=LocalDateTime.now();
+    }
+}
