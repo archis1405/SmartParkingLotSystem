13ce152305693fc94b9703c290bf94c7a12ce173
archis1405 <archismandas1405@gmail.com>
2025-10-19 01:11:54 +0530

    Fixed the Parking floor Repository

--- src/main/java/com/SmartParkingLotSystem/SmartParkingLotSystem/repository/ParkingFloorRepository.java
+++ src/main/java/com/SmartParkingLotSystem/SmartParkingLotSystem/repository/ParkingFloorRepository.java
@@ -4,6 +4,9 @@ import com.SmartParkingLotSystem.SmartParkingLotSystem.entity.ParkingFloor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ParkingFloorRepository extends JpaRepository<ParkingFloor , Long> {
+    List<ParkingFloor> findByParkingLotId(Long parkingLotId);
 }
