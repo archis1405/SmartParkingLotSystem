f065c2d3a60651a88b7ed71255f68673ee70b790
archis1405 <archismandas1405@gmail.com>
2025-10-18 19:00:38 +0530

    First Commit setting up the spring project

---
+++ src/test/java/com/SmartParkingLotSystem/SmartParkingLotSystem/SmartParkingLotSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartParkingLotSystem.SmartParkingLotSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartParkingLotSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
