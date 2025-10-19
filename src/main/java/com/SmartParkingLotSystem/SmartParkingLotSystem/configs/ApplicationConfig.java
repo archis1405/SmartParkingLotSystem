0959a5194b78ffe9174d15928eb8552fcd04b9ac
archis1405 <archismandas1405@gmail.com>
2025-10-20 02:48:02 +0530

    Added the Configurations

---
+++ src/main/java/com/SmartParkingLotSystem/SmartParkingLotSystem/configs/ApplicationConfig.java
@@ -0,0 +1,11 @@
+package com.SmartParkingLotSystem.SmartParkingLotSystem.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableAspectJAutoProxy
+public class ApplicationConfig {
+}
